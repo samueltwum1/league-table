3b1084ed73551a3c775bfcd5c5da6cf3494ff7ce
samueltwum1 <samueltwum1@gmail.com>
2022-08-23 07:44:24 +0200

    Added the test
    Fleshed out the tests first to drive the implementation. Expect tests
    to fail until details are fleshed out

---
+++ src/league_table/leader_board.py
@@ -0,0 +1,14 @@
+"""Determine rank of teams based on match results"""
+
+from typing import List
+
+
+def determine_match_points(match_result: str):
+    pass
+
+def generate_league_table(file_data: List):
+    pass
+
+def rank_league_table(league_table: dict):
+    pass
+
